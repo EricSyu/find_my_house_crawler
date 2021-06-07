a60963e62184b597cbc3153f660aeae2da4d8b9c
WeiJhe <sky820902@gmail.com>
2021-06-08 01:59:56 +0800

    add finish log

--- main.py
+++ main.py
@@ -1,8 +1,8 @@
+import logging
+import os, sentry_sdk, seqlog
 from house_db_model import HouseDbWriter
 from fno_crawler import FiveNineOneCrawler
-import sentry_sdk
 from dotenv import load_dotenv
-import os
 
 if __name__ == '__main__':
     load_dotenv()
@@ -10,10 +10,17 @@ if __name__ == '__main__':
         "https://4834efea1ee74da5ab0602b7454dd30b@o796183.ingest.sentry.io/5802006",
         traces_sample_rate=1.0
     )
+    seqlog.log_to_seq(
+        server_url="http://192.168.80.200:5341/", 
+        level=logging.INFO,
+        batch_size=10,
+        auto_flush_timeout=10,  # seconds
+        override_root_logger=True
+    )
 
     crawler = FiveNineOneCrawler()
     fno_url = os.getenv('FNO_SEARCH_URL')
     houses = crawler.get_houses(fno_url)
 
     HouseDbWriter.insert(houses)
-    
+    logging.info("house-crawlers run finish.")
